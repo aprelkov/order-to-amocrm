34614c823f9ed550f00a41b040c97279d8a84c12
Alex Aprelkov <9720009@mail.ru>
2015-12-24 18:49:52 +0300

    Update lead_add.php

--- lead_add.php
+++ lead_add.php
@@ -1,1 +1,78 @@
-<?php$lead=array(    'name'=>$data['id'],    'price'=>$sum    );if(!empty($title1))	$lead['custom_fields'][]=array(		'id'=>'798180',		'values'=>array(			array(				'value'=>$title1			)		));if(!empty($title2))	$lead['custom_fields'][]=array(		'id'=>'801558',		'values'=>array(			array(				'value'=>$title2			)		));if(!empty($title3))	$lead['custom_fields'][]=array(		'id'=>'807078',		'values'=>array(			array(				'value'=>$title3			)		));if(!empty($title4))	$lead['custom_fields'][]=array(		'id'=>'807080',		'values'=>array(			array(				'value'=>$title4			)		));		$set['request']['leads']['add'][]=$lead;#Формируем ссылку для запроса$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/set';$curl=curl_init(); #Сохраняем дескриптор сеанса cURL#Устанавливаем необходимые опции для сеанса cURLcurl_setopt($curl,CURLOPT_RETURNTRANSFER,true);curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');curl_setopt($curl,CURLOPT_URL,$link);curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($set));curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));curl_setopt($curl,CURLOPT_HEADER,false);curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);CheckCurlResponse($code);/** * Данные получаем в формате JSON, поэтому, для получения читаемых данных, * нам придётся перевести ответ в формат, понятный PHP */$Response=json_decode($out,true);$Response=$Response['response']['leads']['add']; $output='ID добавленных сделок:'.PHP_EOL;foreach($Response as $v)	if(is_array($v))		$output.=$v['id'].PHP_EOL;		$lead_id = $v['id'];return $output;?>
+<?php
+$lead=array(
+    'name'=>$data['id'],
+    'price'=>$sum
+    );
+if(!empty($title1))
+	$lead['custom_fields'][]=array(
+		'id'=>'798180', #замените на id вашего дополнительного поля в amoCRM
+		'values'=>array(
+			array(
+				'value'=>$title1
+			)
+		)
+);
+if(!empty($title2))
+	$lead['custom_fields'][]=array(
+		'id'=>'801558', #замените на id вашего дополнительного поля в amoCRM
+		'values'=>array(
+			array(
+				'value'=>$title2
+			)
+		)
+);
+if(!empty($title3))
+	$lead['custom_fields'][]=array(
+		'id'=>'807078', #замените на id вашего дополнительного поля в amoCRM
+		'values'=>array(
+			array(
+				'value'=>$title3
+			)
+		)
+);
+if(!empty($title4))
+	$lead['custom_fields'][]=array(
+		'id'=>'807080', #замените на id вашего дополнительного поля в amoCRM
+		'values'=>array(
+			array(
+				'value'=>$title4
+			)
+		)
+);
+		
+$set['request']['leads']['add'][]=$lead;
+
+#Формируем ссылку для запроса
+$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/set';
+$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
+#Устанавливаем необходимые опции для сеанса cURL
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
+curl_setopt($curl,CURLOPT_URL,$link);
+curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
+curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($set));
+curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+curl_setopt($curl,CURLOPT_HEADER,false);
+curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+
+$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+CheckCurlResponse($code);
+
+/**
+ * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+ * нам придётся перевести ответ в формат, понятный PHP
+ */
+$Response=json_decode($out,true);
+$Response=$Response['response']['leads']['add'];
+ 
+$output='ID добавленных сделок:'.PHP_EOL;
+foreach($Response as $v)
+	if(is_array($v))
+		$output.=$v['id'].PHP_EOL;
+		$lead_id = $v['id'];
+return $output;
+?>
