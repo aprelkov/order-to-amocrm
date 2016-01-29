194ebd05ba3203b0a1641161497978610b023e79
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 10:59:50 +0300

    Update order_jc.php

--- order_jc.php
+++ order_jc.php
@@ -6,6 +6,6 @@ require $root.'auth.php'; # There will be user authentication
 require $root.'account_current.php'; # Here we will receive information about account
 require $root.'fields_info.php'; # Obtain information about the fields
 require $root.'contacts_list.php'; # Obtain contact information
-require $root.'lead_add.php'; # There will be adding a Laed
-require $root.'contact_add.php'; # There will be adding a Contact lenked with the Lead
+require $root.'lead_add.php'; # There will be adding a Lead
+require $root.'contact_add.php'; # There will be adding a Contact linked with the Lead
 ?>
