21c0e72d732d00498d09dff0d5f5f21dc96fe857
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 10:59:28 +0300

    Update paid_jc.php

--- paid_jc.php
+++ paid_jc.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(-1);
 $root=__DIR__.DIRECTORY_SEPARATOR;
-require $root.'prepare.php'; #Здесь будут производиться подготовительные действия, объявления функций и т.д.
-require $root.'auth.php'; #Здесь будет происходить авторизация пользователя
-require $root.'account_current.php'; #Здесь мы будем получать информацию об аккаунте
-require $root.'fields_info.php'; #Получим информацию о полях
-require $root.'leads_list.php'; #Ищем сделку с нужным нам названием
-require $root.'lead_update.php'; #Здесь будет происходить добавление сделки
+require $root.'prepare.php'; # There will be carried out preparatory acts, declarations of functions, etc.
+require $root.'auth.php'; # There will be user authentication
+require $root.'account_current.php'; # Here we will receive information about account
+require $root.'fields_info.php'; # Obtain information about the fields
+require $root.'leads_list.php'; # Obtain lead information
+require $root.'lead_update.php'; # There will be updating the Lead
 ?>
