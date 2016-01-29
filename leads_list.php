c035b9c96abf5b2b3c31d5573e2ccb44e9db32b1
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 12:08:29 +0300

    Update leads_list.php

--- leads_list.php
+++ leads_list.php
@@ -1,8 +1,8 @@
 <?php
-$lname=$data['id'];
-$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/list?query='.$lname;
-$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-#Устанавливаем необходимые опции для сеанса cURL
+$lead_name=$data['id'];
+$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/list?query='.$lead_name;
+$curl=curl_init(); # Save the cURL session handle
+# Set the necessary options for cURL session
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
 curl_setopt($curl,CURLOPT_URL,$link);
@@ -12,7 +12,7 @@ curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6
 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
  
-$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$out=curl_exec($curl); # Initiate a request to the API and stores the response to variable
 $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
 CheckCurlResponse($code);
 
